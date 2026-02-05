bf5dcef2759ed95c3358c4b085cd9a65983c96cc
Jerome Pacaud <j.pacaud.formation@protonmail.com>
2026-02-05 11:41:55 +0100

    jour-04: Eloquent ORM

--- database/migrations/2026_02_03_140521_products.php
+++ database/migrations/2026_02_03_140521_products.php
@@ -14,9 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')
-                ->nullable()
-                ->constrained()
-                ->nullOnDelete();
+                ->constrained('categories');
             /*
                 Équivalent à :
                 $table->unsignedBigInteger('category_id');
