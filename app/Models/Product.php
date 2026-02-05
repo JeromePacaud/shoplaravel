6150cae3ea741aa77160311495ea0453a692cb35
Jerome Pacaud <j.pacaud.formation@protonmail.com>
2026-02-05 11:51:36 +0100

    jour-04: Eloquent ORM Exercice 1:   - Création des models Category et Product   - Utilisation de la commande php artisant make:model NomModel

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'stock',
+        'category_id',
+        'image',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'active' => 'boolean',
+    ];
+}
