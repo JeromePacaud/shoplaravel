8ae3b26b88ceff3f3d4a4e0340a957c595efc97a
Jerome Pacaud <j.pacaud.formation@protonmail.com>
2026-02-06 16:44:52 +0100

    Bonnus:
    -exirce 1. 2 3 jour 5

---
+++ app/Http/Controllers/ProductController2.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ProductController2 extends Controller
+{
+    public function index(Request $request) {
+        $products = Product::all();
+        $routeName  = $request->route()->getName();
+        $context = [
+            'title' => 'Products List',
+            'products' => $products,
+            'routeName' => $routeName
+        ];
+        return view('products.index', $context);
+    }
+    public function show(Request $request, Product $product): View {
+        $routeName = $request->route()->getName();
+        $context = [
+            'product' => $product,
+            'routeName' => $routeName
+        ];
+        return view('products.product_detail', $context);
+    }
+}
